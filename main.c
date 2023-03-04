7b7eecd90aec769daad058f0f11dc6a1037c3ea4
MatheusCamargoGinebro <matheuscginrbro@gmail.com>
2023-03-04 13:56:52 -0300

    add structs

--- main.c
+++ main.c
@@ -2,6 +2,49 @@
 #include <stdio.h>
 #include <locale.h>
 
+/*-------------------------------</> Structs </>-------------------------------*/
+
+// Estrutura de data:
+typedef struct
+{
+    int day;
+    int month;
+    int year;
+}date;
+
+
+// Estrutura de dados:
+typedef struct
+{
+    int cadNum;
+    int fabricante;
+    int modelo;
+    float cilindrada;
+    char Dono[128];
+    char problema[2048];
+    date data;
+}dados;
+
+
+// Estrutura de node:
+typedef struct NODE
+{
+    dados moto;
+
+    struct NODE *proximo;
+}node;
+
+
+// Estrutura de lista:
+typedef struct
+{
+    node *inicio;
+
+    node *fim;
+    
+    int cont;
+}list;
+
 
 int main()
 {
