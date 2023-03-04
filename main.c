120e4f3fe9c1efb5e0bd356c8dccf63d6c558b26
MatheusCamargoGinebro <matheuscginrbro@gmail.com>
2023-03-04 13:57:56 -0300

    add Functions & main

--- main.c
+++ main.c
@@ -41,14 +41,77 @@ typedef struct
     node *inicio;
 
     node *fim;
-    
+
     int cont;
 }list;
 
 
+
+/*-------------------------------</> Functions </>-------------------------------*/
+void proxtela(){
+    printf("\n\n");
+    system("pause");
+    system("cls");
+}
+
 int main()
 {
     setlocale(LC_ALL, "portuguese");
+
+    //Variável de resposta.
+    int rUser;
+
+    do
+    {
+        printf("O================================================O\n");
+        printf("| Programa de administração da oficina do seu zé |\n");
+        printf("O================================================O\n");
+        printf("|           [1] Listar todas as motos.           |\n");
+        printf("|           [2] Cadastrar nova moto.             |\n");
+        printf("|           [3] Buscar por modelo.               |\n");
+        printf("|           [4] Sair do programa.                |\n");
+        printf("O================================================O\n\nR: ");
+    
+        scanf("%d", &rUser);
+
+        switch (rUser){
+            case 1:
+                printf("O==========================================O\n");
+                printf("| Você escolheu [1] Listar todas as motos. |\n");
+                printf("O==========================================O");
+
+
+            break;
+
+            case 2:
+                printf("O========================================O\n");
+                printf("| Você escolheu [2] Cadastrar nova moto. |\n");
+                printf("O========================================O\n");
+                
+            break;
+
+            case 3:
+                printf("O======================================O\n");
+                printf("| Você escolheu [3] Buscar por modelo. |\n");
+                printf("O======================================O\n");             
+                
+            break;
+
+            case 4:
+                printf("O=====================================O\n");
+                printf("| Você escolheu [4] Sair do programa. |\n");
+                printf("O=====================================O\n");
+                
+            break;
+            
+            default:
+                printf("\n\nOpção inválida, escolha entre 1 e 4.");
+                
+            break;
+        }
+    
+        proxtela();
+    } while (rUser!=4);
     
 
 
