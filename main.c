75b5f81161afc680d868c86f949ea3b7ac3ac703
MatheusCamargoGinebro <matheuscginrbro@gmail.com>
2023-03-05 10:58:07 -0300

    add musical notes.

--- main.c
+++ main.c
@@ -2,6 +2,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <locale.h>
+#include <windows.h>
+
+
+// Não necessário para a realização da atividade, mas serve para soltar sons pelo terminal, e isso deixa o programa mais bonitinho.
+#define C 132.000
+#define Cs 139.788
+#define D 148.104
+#define E 166.320
+#define F 176.220
+#define G 197.736
+#define A 222.024
+#define B 249.216
 
 /*-------------------------------</> Structs </>-------------------------------*/
 
@@ -98,6 +110,7 @@ list *CreateList()
     if (lista == NULL)
     { // Houve erro.
         printf("Erro: problema no alocamento da memória para a lista.\n");
+        Beep(Cs*3, 1000);
         exit(1);
     }
     else
@@ -139,6 +152,7 @@ void addBike(list *lista, node *bike)
     if (newBike == NULL)
     {
         printf("Erro ao alocar memória para a nova moto.\n");
+        Beep(Cs*3, 1000);
         proxtela();
         exit(1);
     }
@@ -197,7 +211,7 @@ int main()
     do
     {
         printf("O================================================O\n| Programa de administração da oficina do seu zé |\nO================================================O\n|           [1] Listar todas as motos.           |\n|           [2] Cadastrar nova moto.             |\n|           [3] Buscar por modelo.               |\n|           [4] Sair do programa.                |\nO================================================O\n\nR: ");
-
+        Beep(C*3, 500);
         scanf("%d", &rUser);
 
         switch (rUser)
@@ -209,10 +223,12 @@ int main()
             if (bikeList->inicio == NULL)
             {
                 printf("Ainda não há motos na lista, portanto, não há nada para mostrar.\n");
+                Beep(Cs*3, 500);
             }
             else
             {
                 printList(bikeList);
+                Beep(E*3, 500);
             }
 
             break;
@@ -224,6 +240,7 @@ int main()
             node *newbike = (node *)malloc(sizeof(node));
 
             printf("\nDigite o nome da fabricante da moto.\nR: ");
+            Beep(A*3, 500);
             setbuf(stdin, NULL);
             fgets(newbike->moto.fabricante, 128, stdin);
 
@@ -240,6 +257,7 @@ int main()
             {
                 verif = 0;
                 printf("\nDigite o modelo da moto.\nR: ");
+                Beep(G*3, 500);
                 setbuf(stdin, NULL);
                 fgets(newbike->moto.modelo, 128, stdin);
 
@@ -255,11 +273,13 @@ int main()
                 if (modelFinder(bikeList, newbike->moto.modelo, newbike, 0) == 1)
                 {
                     printf("\nModelo já cadastrado. Escolha outro modelo.\n");
+                    Beep(Cs*3, 500);
                     verif = 1;
                 }
             } while (verif);
 
             printf("\nDigite a cor da sua moto.\nR: ");
+            Beep(F*4, 500);
             setbuf(stdin, NULL);
             fgets(newbike->moto.cor, 64, stdin);
 
@@ -273,6 +293,7 @@ int main()
             }
 
             printf("\nDigite o nome do dono da moto.\nR: ");
+            Beep(E*4, 500);
             setbuf(stdin, NULL);
             fgets(newbike->moto.dono, 128, stdin);
 
@@ -286,6 +307,7 @@ int main()
             }
 
             printf("\nDigite o problema que a moto deu.\nR: ");
+            Beep(C*4, 500);
             setbuf(stdin, NULL);
             fgets(newbike->moto.problema, 2048, stdin);
 
@@ -299,24 +321,31 @@ int main()
             }
 
             printf("\nDigite o ano da moto.\nR: ");
+            Beep(A*3, 500);
             scanf("%d", &newbike->moto.ano);
 
             printf("\nDigite o número de cadastro da moto.\nR: ");
+            Beep(F*3, 500);
             scanf("%d", &newbike->moto.cadNum);
 
             printf("\nDigite quantas cilindradas a moto tem.\nR: ");
+            Beep(E*3, 500);
             scanf("%f", &newbike->moto.cilindrada);
 
             printf("\nDigite o dia em que a moto foi entregue na oficina.\nR: ");
+            Beep(D*3, 500);
             scanf("%d", &newbike->moto.data.day);
 
             printf("\nDigite o mês em que a moto foi entregue na oficina.\nR: ");
+            Beep(E*3, 500);
             scanf("%d", &newbike->moto.data.month);
 
             printf("\nDigite o ano em que a moto foi entregue na oficina.\nR: ");
+            Beep(G*3, 500);
             scanf("%d", &newbike->moto.data.year);
 
             addBike(bikeList, newbike);
+            Beep(C*3, 500);
             break;
 
         case 3:
@@ -325,7 +354,9 @@ int main()
 
             node *bikeEditor = (node *)malloc(sizeof(node));
             char modelToFind[128];
+
             printf("Digite o modelo da moto que você quer encontrar na oficina.\nR: ");
+            Beep(C*4, 500);
             setbuf(stdin, NULL);
             fgets(modelToFind, 128, stdin);
 
@@ -341,26 +372,31 @@ int main()
             if (modelFinder(bikeList, modelToFind, bikeEditor, 1) == 1)
             {
                 printf("\nMoto encontrada:\n\n");
+                Beep(A*3, 500);
                 printBikeDetails(bikeEditor);
                 char rDecision;
 
                 do
                 {
                     printf("\nDeseja editar as informações da moto? [Y/N].\nR: ");
+                    Beep(D*4, 500);
                     setbuf(stdin, NULL);
                     scanf("%c", &rDecision);
 
                     if (rDecision == 'Y' || rDecision == 'y' || rDecision == 'S' || rDecision == 's')
                     {
+                        Beep(F*3, 500);
                         verif = 1;
                     }
                     else if (rDecision == 'N' || rDecision == 'n')
                     {
+                        Beep(D*3, 500);
                         verif = -1;
                     }
                     else
                     {
                         printf("\nOpção inválida. escolha entre [Y/N].\n");
+                        Beep(Cs*3, 500);
                         verif = 0;
                     }
                 } while (verif == 0);
@@ -374,18 +410,20 @@ int main()
                     {
                         system("cls");
                         printf("O=====================================O\n|     Escolha o que deseja editar.    |\nO=====================================O\t -Informações atuais da moto: \n|   [1] Número de cadastro.           |\t   Número de cadastro da moto: %d.\n|   [2] Fabricante.                   |\t   Fabricante da moto: %s.\n|   [3] Modelo.                       |\t   Modelo da moto: %s.\n|   [4] Cilindradas.                  |\t   Cilindradas da moto: %.1f.\n|   [5] Cor.                          |\t   Cor da moto: %s.\n|   [6] Data de entrega na oficina.   |\t   Data em que a moto entrou na oficina: %d/%d/%d.\n|   [7] Ano.                          |\t   Ano de fabricação da moto: %d\n|   [8] Nome do dono.                 |\t   Nome do dono da moto: %s.\n|   [9] Problemas apresentados.       |\t   Problema que a moto apresentou: %s.\n|   [10] Sair da edição.              |\t\nO=====================================O\n\nR: ", bikeEditor->moto.cadNum, bikeEditor->moto.fabricante, bikeEditor->moto.modelo, bikeEditor->moto.cilindrada, bikeEditor->moto.cor, bikeEditor->moto.data.day, bikeEditor->moto.data.month, bikeEditor->moto.data.year, bikeEditor->moto.ano, bikeEditor->moto.dono, bikeEditor->moto.problema);
-
+                        Beep(G*3, 500);
                         scanf("%d", &rUser2);
 
                         switch (rUser2)
                         {
                         case 1:
                             printf("\nDigite um novo número de cadastro.\nR: ");
+                            Beep(C*3, 500);
                             scanf("%d", &bikeEditor->moto.cadNum);
                             break;
 
                         case 2:
                             printf("\nDigite uma nova fabricante.\nR: ");
+                            Beep(D*3, 500);
                             setbuf(stdin, NULL);
                             fgets(bikeEditor->moto.fabricante, 128, stdin);
 
@@ -404,6 +442,7 @@ int main()
                             {
                                 verif=0;
                                 printf("\nDigite o novo modelo da moto.\nR: ");
+                                Beep(E*3, 500);
                                 setbuf(stdin, NULL);
                                 fgets(bikeEditor->moto.modelo, 128, stdin);
 
@@ -420,6 +459,7 @@ int main()
                                     if (strcmp(bikeEditor->moto.modelo, I->moto.modelo) == 0)
                                     {
                                         printf("\nModelo já cadastrado. Escolha outro modelo.\n");
+                                        Beep(Cs*3, 500);
                                         verif=1;
                                     }
                                 }
@@ -429,11 +469,13 @@ int main()
 
                         case 4:
                             printf("\nDigite a nova quantidade de cilindradas da moto.\nR: ");
+                            Beep(F*3, 500);
                             scanf("%f", &bikeEditor->moto.cilindrada);
                             break;
 
                         case 5:
                             printf("\nDigite a nova cor da moto.\nR: ");
+                            Beep(G*3, 500);
                             setbuf(stdin, NULL);
                             fgets(bikeEditor->moto.cor, 64, stdin);
 
@@ -449,22 +491,27 @@ int main()
 
                         case 6:
                             printf("\nDigite o novo dia em que a moto foi entregue na oficina.\nR: ");
+                            Beep(G*3, 500);
                             scanf("%d", &bikeEditor->moto.data.day);
 
                             printf("\nDigite o novo mês em que a moto foi entregue na oficina.\nR: ");
+                            Beep(E*3, 500);
                             scanf("%d", &bikeEditor->moto.data.month);
 
                             printf("\nDigite o novo amp em que a moto foi entregue na oficina.\nR: ");
+                            Beep(C*3, 500);
                             scanf("%d", &bikeEditor->moto.data.year);
                             break;
 
                         case 7:
                             printf("\nDigite o novo ano da moto.\nR: ");
+                            Beep(A*3, 500);
                             scanf("%d", &bikeEditor->moto.ano);
                             break;
 
                         case 8:
                             printf("\nDigite o novo nome do dono da moto.\nR: ");
+                            Beep(B*3, 500);
                             setbuf(stdin, NULL);
                             fgets(bikeEditor->moto.dono, 128, stdin);
 
@@ -480,6 +527,7 @@ int main()
 
                         case 9:
                             printf("\nDigite o novo problema da moto.\nR: ");
+                            Beep(D*3, 500);
                             setbuf(stdin, NULL);
                             fgets(bikeEditor->moto.problema, 2048, stdin);
 
@@ -495,11 +543,14 @@ int main()
 
                         case 10:
                             printf("\nVocê escolheu parar de editar. as informações modificadas serão salvas.\n");
+                            Beep(C*3, 200);
+                            Beep(D*3, 300);
                             proxtela();
                             break;
 
                         default:
                             printf("\nOpção inválida. tente algo entre 9 e 10.\n");
+                            Beep(Cs*3, 500);
                             proxtela();
                             break;
                         }
@@ -511,21 +562,26 @@ int main()
             else
             {
                 printf("\nMoto não encontrada.\n");
+                Beep(Cs*3, 500);
             }
 
             break;
 
         case 4:
             printf("\nVocê escolheu sair do programa.\n\nlimpando a lista.\n\n");
+            Beep(C*3, 200);
+            Beep(D*3, 300);
 
             ClearList(bikeList);
 
             printf("Lista limpa.");
+            Beep(F*3, 500);
 
             break;
 
         default:
             printf("\nOpção inválida, escolha entre 1 e 4.");
+            Beep(Cs*3, 500);
             break;
         }
 
